804da62c357c4cfc4d9f4b99c620b04be493e033
carlsachs <sachscarl@gmail.com>
2020-09-08 02:33:17 -0400

    added basic search form up top, not functional yet

--- shopify_movies/src/App.js
+++ shopify_movies/src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+
+//styling
+import styled from "styled-components";
 import './App.css';
 
-function App() {
+//components
+import SearchBar from "./components/SearchBar";
+
+//styled-components
+const Top = styled.div`
+  width: 100%;
+  height: 500px;
+`;
+
+function App(props) {
   return (
     <div className="App">
+
+      <Top>
+
+        <SearchBar />
+
+      </Top>
 
     </div>
   );
---
+++ shopify_movies/src/components/SearchBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+//styling
+import styled from "styled-components";
+import { Form } from "react-bootstrap";
+
+const Wrapper = styled.div`
+
+`;
+
+const SearchBar = (props) => {
+
+    return (
+        <Wrapper>
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Search Movies</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                    Search movies by title to nominate for The Shoppies.
+                    </Form.Text>
+                </Form.Group>
+            </Form>
+        </Wrapper>
+    )
+}
+
+export default SearchBar;
